d67389fc6e82994b982b5a307d95d5d17f77010f
Jason Tang <119823259+basonbang@users.noreply.github.com>
2024-02-24 20:49:25 +0000

    Removed .map for Calendar and added styles + props to Event

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Event from "./Event"
 
 const Calendar = () => {
   const times = ['8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
@@ -19,9 +20,18 @@ const Calendar = () => {
           </tr>
         </thead>
         <tbody>
-          {times.map((time, index) => (
-            <tr key={index}> 
-              <td className="time">{time}</td>
+          <tr> 
+              <td className="time">8AM</td>
+              <Event event='Fancy Breakfast (10mg fent) 🔥' color='green'/>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">9AM</td>
               <td></td>
               <td></td>
               <td></td>
@@ -29,8 +39,87 @@ const Calendar = () => {
               <td></td>
               <td></td>
               <td></td>
-            </tr>
-          ))}
+          </tr>
+          <tr> 
+              <td className="time">10AM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">11AM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">12PM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">1PM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">2PM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">3PM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">4PM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr> 
+              <td className="time">5PM</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
         </tbody>
       </table>
     </div>
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Event = (props) => {
   return (
-    <td className="Event">
-      <h5> Test Event Name</h5>
+    <td className={'Event ' + props.color}>
+      <h5> {props.event} </h5>
     </td>
   )
 }
+
+export default Event;
